488e99700c5a4756772d11a216dfc2e1af63e5f4
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-09-06 17:42:24 -0400

    use string builder for obfuscator

--- obfuscator.go
+++ obfuscator.go
@@ -1,7 +1,6 @@
 package sqllexer
 
 import (
-	"regexp"
 	"strings"
 )
 
@@ -46,51 +45,52 @@ const (
 // Obfuscate takes an input SQL string and returns an obfuscated SQL string.
 // The obfuscator replaces all literal values with a single placeholder
 func (o *Obfuscator) Obfuscate(input string) string {
-	var obfuscatedSQL string
+	var obfuscatedSQL strings.Builder
 
 	lexer := New(input)
 	for token := range lexer.ScanAllTokens() {
 		switch token.Type {
 		case NUMBER:
-			obfuscatedSQL += NumberPlaceholder
+			obfuscatedSQL.WriteString(NumberPlaceholder)
 		case STRING:
-			obfuscatedSQL += StringPlaceholder
+			obfuscatedSQL.WriteString(StringPlaceholder)
 		case INCOMPLETE_STRING:
-			obfuscatedSQL += StringPlaceholder
+			obfuscatedSQL.WriteString(StringPlaceholder)
 		case IDENT:
 			if o.config.ReplaceDigits {
 				// regex to replace digits in identifier
 				// we try to avoid using regex as much as possible,
 				// as regex isn't the most performant,
 				// but it's the easiest to implement and maintain
-				digits_regex := regexp.MustCompile(`\d+`)
-				obfuscatedSQL += digits_regex.ReplaceAllString(token.Value, "?")
+				obfuscatedSQL.WriteString(digitsRegex.ReplaceAllString(token.Value, "?"))
 			} else {
-				obfuscatedSQL += token.Value
+				obfuscatedSQL.WriteString(token.Value)
 			}
 		case COMMENT:
-			obfuscatedSQL += token.Value
+			obfuscatedSQL.WriteString(token.Value)
 		case MULTILINE_COMMENT:
-			obfuscatedSQL += token.Value
+			obfuscatedSQL.WriteString(token.Value)
 		case DOLLAR_QUOTED_STRING:
-			obfuscatedSQL += "?"
+			obfuscatedSQL.WriteString(StringPlaceholder)
 		case DOLLAR_QUOTED_FUNCTION:
 			if o.config.DollarQuotedFunc {
 				// obfuscate the content of dollar quoted function
 				quotedFunc := strings.TrimPrefix(token.Value, "$func$")
 				quotedFunc = strings.TrimSuffix(quotedFunc, "$func$")
-				obfuscatedSQL += "$func$" + o.Obfuscate(quotedFunc) + "$func$"
+				obfuscatedSQL.WriteString("$func$")
+				obfuscatedSQL.WriteString(o.Obfuscate(quotedFunc))
+				obfuscatedSQL.WriteString("$func$")
 			} else {
 				// treat dollar quoted function as dollar quoted string
-				obfuscatedSQL += "?"
+				obfuscatedSQL.WriteString(StringPlaceholder)
 			}
 		case ERROR | UNKNOWN:
 			// if we encounter an error or unknown token, we just append the value
-			obfuscatedSQL += token.Value
+			obfuscatedSQL.WriteString(token.Value)
 		default:
-			obfuscatedSQL += token.Value
+			obfuscatedSQL.WriteString(token.Value)
 		}
 	}
 
-	return strings.TrimSpace(obfuscatedSQL)
+	return strings.TrimSpace(obfuscatedSQL.String())
 }
